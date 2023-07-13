65fe3c99fbc41cee0cd9da55e428a8d7f602c6bb
sampletestgh <lakshmi.vinu19@gmail.com>
2023-07-13 15:25:03 +0530

    firstcommit-jenkins

---
+++ Jenkins2/src/test/java/JenkinsPractice/SampleJen.java
@@ -0,0 +1,14 @@
+package JenkinsPractice;
+
+import org.testng.annotations.Test;
+
+public class SampleJen {
+	
+	
+	@Test
+	public void metod_contact()
+	{
+		System.out.println("method sucess-----");
+	}
+
+}
